ebfdc9ea1a0405990b264680a532860682819355
Pang Dennis <zixian.pang@uzh.ch>
2024-03-05 17:50:34 +0100

    getDomain updated

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -8,7 +8,7 @@ import { isProduction } from "./isProduction"
  */
 export const getDomain = () => {
   const prodUrl = "https://sopra-fs24-pang-zixian-client.oa.r.appspot.com/" // TODO: insert your prod url for server (once deployed)
-  const devUrl = "http://localhost:8080"
+  const devUrl = "https://sopra-fs24-pang-zixian-server.oa.r.appspot.com"
 
   return isProduction() ? prodUrl : devUrl
 }
