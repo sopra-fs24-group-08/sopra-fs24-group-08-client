e32e38e84d4acfe3d80379d37c23bec4a94d55d0
LooPyt <luis.schmid@uzh.ch>
2024-03-22 18:12:56 +0100

    #M2 Tag assigned?

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -7,7 +7,7 @@ import "../../styles/views/Header.scss";
  * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
  * Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen.
  * They are reusable pieces, and think about each piece in isolation.
- * Functional components have to return always something. However, they don't need a "render()" method.
+ * Functional com ponents have to return always something. However, they don't need a "render()" method.
  * https://react.dev/learn/your-first-component and https://react.dev/learn/passing-props-to-a-component 
  * @FunctionalComponent
  */
